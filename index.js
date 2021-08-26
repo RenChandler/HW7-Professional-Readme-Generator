4ae23ceaa3a7a6943427db26b029d0f7dfea28e6
Ren <rnchandramouli42@gmail.com>
2021-08-26 12:43:26 -0600

    Added file creation system

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
 const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require('fs')
+const path = require ('path')
 // TODO: Create an array of questions for user input
 const questions = [
     // Title Question
@@ -62,7 +64,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(fileName),data)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
